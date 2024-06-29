55c8e077ed8465eda8a0a50503aa1c4135da3d1c
fatkhulizz <fatkhul7izza@gmail.com>
2024-06-29 07:31:14 +0700

    added some keymaps

--- lua/kickstart/plugins/todo-comments.lua
+++ lua/kickstart/plugins/todo-comments.lua
@@ -1,5 +1,30 @@
 -- Highlight todo, notes, etc in comments
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+    keys = {
+      {
+        ']t',
+        function()
+          require('todo-comments').jump_next()
+        end,
+        desc = 'Next Todo Comment',
+      },
+      {
+        '[t',
+        function()
+          require('todo-comments').jump_prev()
+        end,
+        desc = 'Previous Todo Comment',
+      },
+      { '<leader>xt', '<cmd>Trouble todo toggle<cr>', desc = '[t]odo (Trouble)' },
+      { '<leader>xT', '<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>', desc = '[T]odo/Fix/Fixme (Trouble)' },
+      { '<leader>st', '<cmd>TodoTelescope<cr>', desc = '[t]odo' },
+      { '<leader>sT', '<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>', desc = '[T]odo/Fix/Fixme' },
+    },
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
