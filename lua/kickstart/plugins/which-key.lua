a9b20687460669c3deb3dc9365c51bf7b1e8e747
fatkhulizz <fatkhul7izza@gmail.com>
2024-06-29 07:30:59 +0700

    added some registers

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -23,16 +23,30 @@ return {
       -- Document existing key chains
       require('which-key').register {
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+        -- ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
         ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+
+        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+        ['<leader>x'] = { name = '(Trouble)', _ = 'which_key_ignore' },
+        ['<leader>d'] = { name = '[D]iagnostic', _ = 'which_key_ignore' },
+        ['<leader>q'] = { name = '[Q]uit', _ = 'which_key_ignore' },
+        ['<leader>ss'] = { name = '[S]earch [S]ymbol', _ = 'which_key_ignore' },
+        ['<leader><tab>'] = { name = '[tab]', _ = 'which_key_ignore' },
+        -- hide this
+        ['<leader>y'] = { 'which_key_ignore' },
+        ['<leader>Y'] = { 'which_key_ignore' },
       }
       -- visual mode
       require('which-key').register({
         ['<leader>h'] = { 'Git [H]unk' },
+        -- hide this
+        ['<leader>d'] = { 'which_key_ignore' },
+        ['<leader>y'] = { 'which_key_ignore' },
+        ['<leader>p'] = { 'which_key_ignore' },
       }, { mode = 'v' })
     end,
   },
